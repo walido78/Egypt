99acd90758ce95b2de17eefba8b68af9560c4232
Walid Hemaya <hemaya.walid@hotmail.fr>
2023-03-19 15:07:36 +0100

    Adding Firebase Auth

---
+++ src/screens/Favourite/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#888',
+    textAlign: 'center',
+  },
+});
---
+++ src/screens/LoginScreen/index.js
@@ -0,0 +1,66 @@
+
+import firebase from '../../../firebase.js';
+
+
+
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+import styles from './styles';
+
+const LoginScreen = ({ navigation }) => {
+    const handleLoginPress = () => {
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(email, password)
+          .then(() => {
+            // Navigate to the home screen or other authenticated screen
+            navigation.navigate('Home');
+          })
+          .catch((error) => {
+            // Handle authentication errors here
+          });
+      };
+
+  const handleRegisterPress = () => {
+    // navigate to the login page when the login button is pressed
+    navigation.navigate('Register');
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.logo}>Egypt Recipes</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email..."
+          placeholderTextColor="#003f5c"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Password..."
+          placeholderTextColor="#003f5c"
+          secureTextEntry
+        />
+      </View>
+      <TouchableOpacity style={styles.loginBtn} onPress={handleLoginPress}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleRegisterPress}>
+        <Text style={styles.signupText}>Signup</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default LoginScreen;
